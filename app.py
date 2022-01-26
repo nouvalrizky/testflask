294e8c1e346319e3e065480696912047255dc5cb
Nouval <nouvalr@gmail.com>
2022-01-26 09:51:45 +0800

    fix app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
 
-if __name__ == '__main__':
-    app.run()
+application = app
+
+@app.route("/")
+
+def hello():
+    return "This is Hello World!\n"
+
+if __name__ == "__main__":
+    app.run(debug=True)
